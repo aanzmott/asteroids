115d2f4d073056f6da61d7ce9b34b196b7fe37a5
aanzmtt <aanmott@gmail.com>
2025-08-10 15:04:21 -0400

    0.3.5 smol boom

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from constants import *
 from circleshape import *
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        
+        split_to = random.uniform( 20, 50)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = self.velocity.rotate(split_to) * 1.2
+        asteroid2.velocity = self.velocity.rotate(-split_to) * 1.2
--- main.py
+++ main.py
@@ -52,11 +52,11 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
-                
+
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
                     shot.kill() 
+                    asteroid.split()
 
         # spawn new asteroids
         for obj in drawable:
